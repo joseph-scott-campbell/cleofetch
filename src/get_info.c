b33115afa2160d5b9168ca46e0acc10cef697f78
Scott Campbell <scott@josephscottcampbell.com>
2024-09-09 12:49:52 -0400

    added freebsd support for getting memory

--- src/get_info.c
+++ src/get_info.c
@@ -6,15 +6,38 @@
 #include "config.h" // contains definitions that should be configured by user
 #include "get_info.h"
 
+#ifdef FREE_BSD
+    #include <sys/sysctl.h>
+#endif
+
 static int get_mem_info(struct mem_t *mem){
-    long pages = sysconf(_SC_PHYS_PAGES);
-    long free_pages = sysconf(_SC_AVPHYS_PAGES);
+    long pages = sysconf(_SC_PHYS_PAGES); // get total pages
+ 
+    // if OS is freebsd (needs to be defined in config.h) then use the freebsd
+    // version of getting memeory
+    #ifdef FREE_BSD
+        long free_pages;
+        size_t len = sizeof(free_pages);
+        if (sysctlbyname("vm.stats.vm.v_free_count", &free_pages, &len,
+                    NULL, 0) == -1) {
+            printf("Error: getting free ram through sysctl\n");
+            return 1;
+        }
+    #endif
+
+    // if not freebsd use the linux version
+    #ifndef FREE_BSD
+        long free_pages = sysconf(_SC_AVPHYS_PAGES);
+    #endif
+
     long page_size = sysconf(_SC_PAGE_SIZE);
 
+    // if anything errored return error to get_info where it will print
     if(pages == -1 || free_pages == -1 || page_size == -1){
         return 1;
     }
     
+    // use page information to calculate
     mem->total = (pages * page_size)/1000000;
     mem->free = (free_pages * page_size)/1000000;
     
@@ -22,7 +45,7 @@ static int get_mem_info(struct mem_t *mem){
 }
 
 int get_info(struct info_t *info){
-
+    // take direct pointer to environment variable from shell
     info->shell = getenv("SHELL");
     info->terminal = getenv("TERM");
     info->user = getlogin();
